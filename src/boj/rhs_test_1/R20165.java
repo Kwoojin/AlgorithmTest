2a5106d5311b5a18837c7f10080f1c02d998baff
WoojinKim <dnwls488@naver.com>
2022-05-15 23:24:52 +0900

    문제풀이

--- src/boj/rhs_test_1/R20165.java
+++ src/boj/rhs_test_1/R20165.java
@@ -7,14 +7,21 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class R20165 {
 	static int N,M, answer=0;
 	static int[][] graph;
 	static char[][] ch;
-	
+
 	static int[] dx = {0, 0, 1, -1};
 	static int[] dy = {1, -1, 0, 0};
-	
+
 	static void attack(int i, int j, int size, int ewsn) {
 		if( i>=0 && i<N && j>=0 && j<M && size>0) {
 			if(ch[i][j] == 'S') {
@@ -27,23 +34,23 @@ public class R20165 {
 			attack(ni, nj, size-1, ewsn);
 		}
 	}
-	
+
 	static void defense(int i, int j) {
 		ch[i][j] = 'S';
 	}
-	
+
 	static int direction(char ewsn) {
 		int next;
 		if(ewsn == 'E') next=0;
 		else if(ewsn == 'W') next=1;
 		else if(ewsn == 'S') next=2;
 		else next=3;
-			
+
 		return next;
 	}
 
 	public static void main(String[] args) {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		try {
@@ -53,7 +60,7 @@ public class R20165 {
 			int R = Integer.parseInt(st.nextToken());
 			graph = new int[N][M];
 			ch = new char[N][M];
-			
+
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j=0; j<M; j++) {
@@ -61,20 +68,20 @@ public class R20165 {
 					ch[i][j] = 'S';
 				}
 			}
-			
+
 			for(int v=0; v<R; v++) {
 				st = new StringTokenizer(br.readLine());
 				int i = Integer.parseInt(st.nextToken())-1;
 				int j = Integer.parseInt(st.nextToken())-1;
 				int ewsn = direction(st.nextToken().charAt(0));
 				attack(i, j, graph[i][j], ewsn);
-				
+
 				st = new StringTokenizer(br.readLine());
 				i = Integer.parseInt(st.nextToken())-1;
 				j = Integer.parseInt(st.nextToken())-1;
 				defense(i, j);
 			}
-			
+
 			bw.write(String.valueOf(answer));
 			bw.write('\n');
 			for(int i=0; i<N; i++) {
@@ -83,10 +90,10 @@ public class R20165 {
 				}
 				bw.write('\n');
 			}
-			
+
 			bw.flush();
 			bw.close();
-			
+
 		} catch(IOException e) {
 		}
 	}
--- src/boj/rhs_test_1/R20166.java
+++ src/boj/rhs_test_1/R20166.java
@@ -1,93 +1,87 @@
 package boj.rhs_test_1;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class R20166 {
-	static int N,M, answer=0;
-	static int[][] graph;
-	static char[][] ch;
-	
-	static int[] dx = {0, 0, 1, -1};
-	static int[] dy = {1, -1, 0, 0};
-	
-	static void attack(int i, int j, int size, int ewsn) {
-		if( i>=0 && i<N && j>=0 && j<M && size>0) {
-			if(ch[i][j] == 'S') {
-				ch[i][j]='F';
-				answer++;
-				size = Math.max(size, graph[i][j]);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M, K;
+	static char[][] graph;
+	static HashMap<String, Integer> result = new HashMap<>();
+
+	static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+	static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+	static void DFS(int x, int y, String str) {
+		str = str+graph[x][y];
+		int L = str.length();
+		result.put(str, result.getOrDefault(str, 0)+1);
+		if(L==5) {
+			return;
+		} else {
+			for(int i=0; i<dx.length; i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				if(nx == -1) nx = N-1;
+				if(nx == N) nx = 0;
+				if(ny == -1) ny = M-1;
+				if(ny == M) ny = 0;
+
+				DFS(nx,ny,str);
 			}
-			int ni = i+dx[ewsn];
-			int nj = j+dy[ewsn];
-			attack(ni, nj, size-1, ewsn);
 		}
 	}
-	
-	static void defense(int i, int j) {
-		ch[i][j] = 'S';
-	}
-	
-	static int direction(char ewsn) {
-		int next;
-		if(ewsn == 'E') next=0;
-		else if(ewsn == 'W') next=1;
-		else if(ewsn == 'S') next=2;
-		else next=3;
-			
-		return next;
-	}
+
 
 	public static void main(String[] args) {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		input();
+		process();
+
+	}
+
+	static void process(){
+		try {
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<M; j++) {
+					DFS(i, j, "");
+				}
+			}
+
+			for(int p=0; p<K; p++) {
+				String prob = br.readLine();
+				bw.write(String.valueOf(result.get(prob)));
+				bw.write('\n');
+			}
+			bw.flush();
+			bw.close();
+
+		} catch(IOException e) {
+
+		}
+	}
+
+	static void input(){
 		try {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			int R = Integer.parseInt(st.nextToken());
-			graph = new int[N][M];
-			ch = new char[N][M];
-			
+			K = Integer.parseInt(st.nextToken());
+
+			graph = new char[N][M];
 			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());
+				String tmp = br.readLine();
 				for(int j=0; j<M; j++) {
-					graph[i][j] = Integer.parseInt(st.nextToken());
-					ch[i][j] = 'S';
+					graph[i][j] = tmp.charAt(j);
 				}
 			}
-			
-			for(int v=0; v<R; v++) {
-				st = new StringTokenizer(br.readLine());
-				int i = Integer.parseInt(st.nextToken())-1;
-				int j = Integer.parseInt(st.nextToken())-1;
-				int ewsn = direction(st.nextToken().charAt(0));
-				attack(i, j, graph[i][j], ewsn);
-				
-				st = new StringTokenizer(br.readLine());
-				i = Integer.parseInt(st.nextToken())-1;
-				j = Integer.parseInt(st.nextToken())-1;
-				defense(i, j);
-			}
-			
-			bw.write(String.valueOf(answer));
-			bw.write('\n');
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<M; j++) {
-					bw.write(String.valueOf(ch[i][j]) + ' ');
-				}
-				bw.write('\n');
-			}
-			
-			bw.flush();
-			bw.close();
-			
+
 		} catch(IOException e) {
+
 		}
 	}
 }
